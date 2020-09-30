a5ea6da47b9ef0579278a15c2d3816f017842b2c
postila <isabella.postila@hotmail.com>
2020-09-30 19:34:15 +0200

    Added living method to make an animal die if health level reach 0. And a method to test it it in Main.

--- src/Animal.java
+++ src/Animal.java
@@ -5,4 +5,23 @@ public abstract class Animal {
         FEMALE,
         MALE
     }
+    public String name;
+    public Gender gender;
+    public double health = 100.0; // Health set to 100% from beginning.
+    public boolean isAlive = true;
+
+    public void living(){
+        System.out.println("Your animals health level is: " + health);
+        if(health > 0){
+            System.out.println("Your animal is still alive.");
+            return;
+        }
+        isAlive = false;
+        System.out.println("Your animal is dead.");
+        // NOTE: remember to remove animal from players list.
+    }
+
+    public void changeHealth(double x){
+        health -= x;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Horse horse = new Horse();
+        horse.living();
+        horse.changeHealth(50);
+        horse.living();
+        horse.changeHealth(26);
+        horse.living();
+        horse.changeHealth(24);
+        horse.living();
     }
 }
