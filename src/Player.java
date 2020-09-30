e60c05ebacb812d27a9cc53e6af71dc99f8241a1
postila <isabella.postila@hotmail.com>
2020-09-30 21:15:47 +0200

    Updated Player class field attributes. Added addAnimal and showAnimal methods, to check if it is working as expected.

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
 
 public class Player {
-    private String name;
-    private int money;
+    public String name;
+    public int money;
+    public ArrayList<Animal> animals;
+    public ArrayList<Food> foods;
+
+    public Player(){
+        this.animals = new ArrayList<>();
+        this.foods = new ArrayList<>();
+    }
+
+    public void addAnimal(Animal animal){
+        animals.add(animal);
+    }
+    public void showAnimal(){
+        for(var a : animals){
+            System.out.println(a.getClass().getSimpleName());
+        }
+    }
 }
