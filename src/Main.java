22bc2d33ed4d51a721d68605c3a65d7f58e6f5fc
postila <isabella.postila@hotmail.com>
2020-09-30 21:16:23 +0200

    Testing methods from Player class.

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         Horse horse = new Horse();
-        horse.decreaseHealth(); //  Randomize a number to decrease from horse life value
-        horse.changeHealth();   //  Change the horse life value
-        System.out.println(horse.health);   //  Print out the new life value
-        System.out.println(horse.increaseHealth()); //  Increase life value with 10%
-
+        Player player1 = new Player();  // Create new player
+        player1.addAnimal(horse);       // Add horse to player1's arraylist of animals
+        player1.showAnimal();           // Loop through & show what animal player have in his/hers list
     }
 }
