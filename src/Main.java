1f61b2623c1cf313bb001aa94b337134aac0f806
postila <isabella.postila@hotmail.com>
2020-09-30 20:54:03 +0200

    Added increaseHealth method. Changed changeHealth method, to set new health value.

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         Horse horse = new Horse();
-
-        horse.healthLoss();
-        horse.healthLoss();
-        horse.healthLoss();
-        horse.healthLoss();
-        horse.healthLoss();
+        horse.decreaseHealth(); //  Randomize a number to decrease from horse life value
+        horse.changeHealth();   //  Change the horse life value
+        System.out.println(horse.health);   //  Print out the new life value
+        System.out.println(horse.increaseHealth()); //  Increase life value with 10%
 
     }
 }
