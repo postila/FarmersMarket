919983c1c3e0bd81d497b605fac3ba43b51a350d
postila <isabella.postila@hotmail.com>
2020-09-30 20:36:58 +0200

    Added a method that calculates a random percentage that animal lose if not fed, and test random in Main.

--- src/Animal.java
+++ src/Animal.java
@@ -9,6 +9,7 @@ public abstract class Animal {
     public Gender gender;
     public double health = 100.0; // Health set to 100% from beginning.
     public boolean isAlive = true;
+    public Random random = new Random();
 
     public void living(){
         System.out.println("Your animals health level is: " + health);
@@ -20,8 +21,16 @@ public abstract class Animal {
         System.out.println("Your animal is dead.");
         // NOTE: remember to remove animal from players list.
     }
-
     public void changeHealth(double x){
         health -= x;
+    }
+    // randomize a percentage between 10-30 for health loss
+    public void healthLoss(){
+        double healthDecrease = random.nextInt(21) + 10;
+        //  Convert the unit of loss into percentage
+        healthDecrease /= 100;
+        // Calculates how much loss an animal should lose
+        healthDecrease = (int)health * healthDecrease;
+        System.out.println("The amount of life that should be lost if animal not fed: " + (int)healthDecrease);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         Horse horse = new Horse();
-        horse.living();
-        horse.changeHealth(50);
-        horse.living();
-        horse.changeHealth(26);
-        horse.living();
-        horse.changeHealth(24);
-        horse.living();
+
+        horse.healthLoss();
+        horse.healthLoss();
+        horse.healthLoss();
+        horse.healthLoss();
+        horse.healthLoss();
+
     }
 }
