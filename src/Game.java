dcd3c9d087f111797abde70c03f79033e301e39f
postila <isabella.postila@hotmail.com>
2020-09-30 21:38:21 +0200

    Added method to store players in Arraylist in Game class

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Game {
+    Scanner scanner = new Scanner(System.in);
+    public int numberOfRounds;
+    public ArrayList<Player> players = new ArrayList<>();
+
+    public Game(){
+        // CONSTRUCTOR
+    }
+
+    public void addPlayer(Player player){
+        this.players.add(player);
+    }
 }
