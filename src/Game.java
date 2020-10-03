6a324b066d977278b7727a6ebeeab1dc5e04a4ed
postila <isabella.postila@hotmail.com>
2020-10-03 12:53:31 +0200

    Added menu to method mainGame

--- src/Game.java
+++ src/Game.java
@@ -16,8 +16,24 @@ public class Game {
         for(int i = 4; i <numberOfRounds; i++){
             for(Player player : players){
                 System.out.println("It is your turn " + player.name + "." +
-                        "\n What would you like to do?");
-                player.showPlayerInfo();
+                        "\n What would you like to do?" +
+                        "\n1. Buy animal" +
+                        "\n2. Buy food" +
+                        "\n3. Feed animal" +
+                        "\n4. Mate animal" +
+                        "\n5. Sell animal");
+                var userChoice = 0;
+                try{
+                    Scanner scanner = new Scanner(System.in);
+                    userChoice = Integer.parseInt(scanner.next());
+                } catch (Exception e){
+                    System.out.println("Something went wrong. Did you choose something from the list?");
+                }
+                switch (userChoice) {
+                    case 1 -> System.out.println("You decided to buy an animal!");
+                    case 2 -> System.out.println("Okay, so you want to buy some food.");
+                    case 3 -> System.out.println("Dinner time!");
+                }
             }
         }
     }
