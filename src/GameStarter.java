80dd20c44890f1f0dada08133a8d0bd5cefe284e
postila <isabella.postila@hotmail.com>
2020-10-05 09:06:10 +0200

    Changing output.

--- src/GameStarter.java
+++ src/GameStarter.java
@@ -2,21 +2,35 @@ import java.util.*;
 
 public class GameStarter{
         Scanner scanner = new Scanner(System.in);
+        public int numberOfPlayers;
+        public int numberOfRounds;
+        public ArrayList<Player> players;
 
         public GameStarter(){
-            var numberOfPlayers = 0;
-            var numberOfRounds = 0;
+
             while(numberOfPlayers > 4 || numberOfPlayers < 1 || numberOfRounds > 30 || numberOfRounds < 5)
                 try {
-                    numberOfPlayers = Integer.parseInt(prompt("Hur många spelare (1-4)?"));
-                    numberOfRounds = Integer.parseInt(prompt("Hur många rundor (5-30)?"));
+                    numberOfPlayers = Integer.parseInt(prompt("How many players are you, (1-4)?"));
+                    numberOfRounds = Integer.parseInt(prompt("How many rounds do you want to play (5-30)?"));
                 }
                 catch(Exception ignore){ }
-            var players = new ArrayList<Player>();
+
+            players = new ArrayList<>();
             for(var i = 1; i <= numberOfPlayers; i++){
-                players.add(new Player(prompt("Spelare " + i + ":s namn?")));
+                if(i==1) {
+                    players.add(new Player(prompt("Enter " + i + "st players name: ")));
+                }
+                if(i==2) {
+                    players.add(new Player(prompt("Enter " + i + "nd players name: ")));
+                }
+                if(i==3) {
+                    players.add(new Player(prompt("Enter " + i + "rd players name: ")));
+                }
+                if(i==4) {
+                    players.add(new Player(prompt("Enter " + i + "th players name: ")));
+                }
             }
-            new Game(players);
+            new Game(players, numberOfRounds);
         }
 
         private String prompt(String question){
