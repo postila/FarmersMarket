9e9b6914556e35a37e2ee71665766485da6f1f7e
postila <isabella.postila@hotmail.com>
2020-10-07 11:35:59 +0200

    Cleaned code and changed it so it can handle unexpected input from user.

--- src/Store.java
+++ src/Store.java
@@ -1,66 +1,65 @@
 import java.util.Scanner;
 
 public class Store {
+    private static final Scanner scanner = new Scanner(System.in);
 
-    public Animal createAnimal(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\t[CHOOSE AN ANIMAL]" +
+    public void createAnimal(Player player){
+        boolean exit = false;
+        while(!exit) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("\t[ ANIMALS FOR SALE ]" +
                     "\n[1] HORSE \t|  50.000 SEK" +
                     "\n[2]  COW \t|  12.000 SEK" +
                     "\n[3]  PIG \t|   1.500 SEK" +
                     "\n[4] SHEEP \t|   1.000 SEK" +
-                    "\n[5] LLAMA \t|  10.000 SEK" );
-        var animalChoice = scanner.nextInt();
-        System.out.println("Choose a name:");
-        var name = scanner.next();
-        System.out.println("Choose a gender, (female/male): ");
-        var gender = scanner.next();
+                    "\n[5] LLAMA \t|  10.000 SEK" +
+                    "\n[6]\t\tEXIT STORE  ");
+            var animalChoice = scanner.nextInt();
+            var name = "";
+            var gender = "";
+            if(animalChoice >= 1 && animalChoice <= 5){
+                name = prompt("What would you like to name your animal?");
+                gender = prompt("Chose gender, (female/male): ");
+            }
 
             switch (animalChoice) {
-                case 1 -> {
-                    return new Horse(name, gender);
-                }
-                case 2 -> {
-                    return new Cow(name, gender);
-                }
-                case 3 -> {
-                    return new Pig(name, gender);
-                }
-                case 4 -> {
-                    return new Sheep(name, gender);
-                }
-                case 5 -> {
-                    return new Llama(name, gender);
-                }
-                default -> System.out.println("You did not chose an animal alternative.");
+                case 1 -> player.addAnimal(new Horse(name, gender));
+                case 2 -> player.addAnimal(new Cow(name,gender));
+                case 3 -> player.addAnimal(new Pig(name, gender));
+                case 4 -> player.addAnimal(new Sheep(name, gender));
+                case 5 -> player.addAnimal(new Llama(name, gender));
+                case 6 -> {System.out.println("Come back soon, " + player.name + "!");
+                exit = true;}
+                default -> System.out.println("Choose an option between 1-6!");
             }
-        return null;
+        }
     }
-    public Food deliverFood(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What food would you like to buy? \n1. Hay\n2. Grass\n3. Grain");
-        try {
-            int foodChoice = Integer.parseInt(scanner.next());
-                switch (foodChoice) {
-                    case 1 -> {
-                        System.out.println("How many kilos of hay do you want to buy?");
-                        var kilos = Integer.parseInt(scanner.next());
-                            return new Hay(kilos);
-                    }
-                    case 2 -> {
-                        System.out.println("How many kilos of grass do you want to buy?");
-                        var kilos = Integer.parseInt(scanner.next());
-                        return new Grass(kilos);
-                    }
-                    case 3 -> {
-                        System.out.println("How many kilos of grain do you want to buy?");
-                        var kilos = Integer.parseInt(scanner.next());
-                        return new Grain(kilos);
-                    }
-                }
-            } catch (Exception e) {
-                System.out.println("You did not choose one of the following food alternatives.");
+    public void deliverFood(Player player) {
+        boolean exit = false;
+        while (!exit){
+            System.out.println("\t\t[ FOOD ] " +
+                    "\n[1] HAY\t\t|  100 SEK/KG" +
+                    "\n[2] GRASS\t|  150 SEK/KG" +
+                    "\n[3] GRAIN\t|  200 SEK/KG" +
+                    "\n[4] \tEXIT STORE");
+        int foodChoice = Integer.parseInt(prompt("What food would you like to buy today?"));
+        int kilos = 0;
+        if (foodChoice >= 1 && foodChoice <= 3) {
+            kilos = Integer.parseInt(prompt("How many kilos would you like to get?"));
+        }
+            switch (foodChoice) {
+                case 1 -> player.addFood(new Hay(kilos));
+                case 2 -> player.addFood(new Grass(kilos));
+                case 3 -> player.addFood(new Grain(kilos));
+                case 4 -> {System.out.println("Welcome back another time!");
+                    exit = true;}
+                default -> System.out.println("Choose an option between 1-4!");
             }
-        return null;
+        }
+    }
+
+    private static String prompt(String question){
+        System.out.println(question);
+        return scanner.nextLine();
     }
 }
