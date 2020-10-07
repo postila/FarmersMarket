38aaaa408b2aa8a707cbd672e77d8a4a1ba18a60
postila <isabella.postila@hotmail.com>
2020-10-07 11:33:29 +0200

    Cleaned code

--- src/Game.java
+++ src/Game.java
@@ -6,12 +6,6 @@ public class Game {
     public ArrayList<Player> players;
     public Store store = new Store();
 
-    ArrayList<Horse> horseList = new ArrayList<>();
-    ArrayList<Cow> cowList = new ArrayList<>();
-    ArrayList<Pig> pigList = new ArrayList<>();
-    ArrayList<Sheep> sheepList = new ArrayList<>();
-    ArrayList<Llama> llamaList = new ArrayList<>();
-
     public Game(ArrayList<Player> players, int numberOfRounds) {
         this.players = players;
         this.numberOfRounds = numberOfRounds;
@@ -19,35 +13,28 @@ public class Game {
     }
 
     public void mainGame() {
-        for (int i = 4; i < numberOfRounds; i++) {
+        for (int i = 1; i < numberOfRounds; i++) {
             for (Player player : players) {
-                System.out.println("It is your turn " + player.name + ". What would you like to do?" +
-                        "\n[1] Buy animal" + "\n[2] Buy food" + "\n[3] Feed animal" + "\n[4] Mate animal" + "\n[5] Sell animal");
+                boolean option = false;
                 int userChoice = 0;
-                try {
-                    Scanner scanner = new Scanner(System.in);
+                while(!option) {
+                    System.out.println("It is your turn " + player.name + ". Make a move!" +
+                            "\n[1] Buy animal" + "\n[2] Buy food" + "\n[3] Feed animal" + "\n[4] Mate animal" + "\n[5] Sell animal");
+
                     userChoice = Integer.parseInt(scanner.next());
                     if (userChoice < 1 || userChoice > 5) {
                         System.out.println("You have to choose a number from the list.");
                     }
-                } catch (Exception e) {
-                    System.out.println("Something went wrong. Pick a number from the menu!");
+                    else {
+                        option = true;
+                    }
                 }
                 if (userChoice != 3) {    // If user don't pick 3, animals will lose life value
                     player.notFeedingAnimal();
                     player.removeDeadAnimal();
                 }
                 switch (userChoice) {
-                    case 1 -> {
-                        while (true) {
-                            buyAnimal(player);  // Creates a new animal and add to players list of animals
-                            System.out.println("Buy another animal? (YES/NO)");
-                            String yesNo = scanner.next();
-                            if (yesNo.toUpperCase().equals("NO")) {
-                                break;
-                            }
-                        }
-                    }
+                    case 1 -> buyAnimal(player);  // Creates a new animal and add to players list of animals
                     case 2 -> buyFood(player);    // Buy food and add to players list
                     case 3 -> {
                         feedAnimal(player);   // Increase animals life value
@@ -61,70 +48,46 @@ public class Game {
     }
 
     public void buyAnimal(Player player) {
-        Animal myNewAnimal = store.createAnimal();
-        player.addAnimal(myNewAnimal);
+        store.createAnimal(player);
     }
-
     public void buyFood(Player player) {
-        boolean buyMore = true;
-        while (buyMore) {
-            Food myNewFood = store.deliverFood();
-            player.addFood(myNewFood);
-            System.out.println("Buy more food? (YES/NO)");
-            String yesNo = scanner.next();
-            if (yesNo.toUpperCase().equals("NO")) {
-                buyMore = false;
-            }
-        }
+        store.deliverFood(player);
     }
     public void feedAnimal (Player player){
-            System.out.println("[ENTER THE TYPE OF FOOD YOU LIKE TO FEED YOUR ANIMALS]");
+        int kilos = 0;
+        int userChoiceOfFood = 0;
+
             for (var f : player.foods) {
                 System.out.println("[" + f.getClass().getSimpleName() + "]");
             }
             try {
-                var userChoiceOfFood = scanner.next();
-                for (var a : player.animals) {
-                    if (userChoiceOfFood.toUpperCase().equals("HAY")) {
-                        if (a instanceof Horse || a instanceof Llama || a instanceof Sheep) {
-                            player.feedHay(a);
-                        }
-                    }
-                    if (userChoiceOfFood.toUpperCase().equals("GRASS")) {
-                        if (a instanceof Cow) {
-                            player.feedGrass(a);
-                        }
-                    }
-                    if (userChoiceOfFood.toUpperCase().equals("GRAIN")) {
-                        if (a instanceof Pig || a instanceof Horse)
-                            player.feedGrain(a);
-                    }
-                }
+                userChoiceOfFood = Integer.parseInt(prompt("Choose what type of food you would like to feed: "));
+                kilos = Integer.parseInt(prompt("How many kilos?"));
             } catch (Exception e) {
                 System.out.println("You gave the wrong input!");
             }
+                for (var a : player.animals) {
+                    if (userChoiceOfFood == 1) {
+                        if (a instanceof Horse || a instanceof Llama || a instanceof Sheep) {
+                            player.feedHay(a, kilos);
+                        }
+                    }
+                    if (userChoiceOfFood == 2) {
+                        if (a instanceof Cow) {
+                            player.feedGrass(a, kilos);
+                        }
+                    }
+                    if (userChoiceOfFood == 3) {
+                        if (a instanceof Pig || a instanceof Horse)
+                            player.feedGrain(a, kilos);
+                    }
+                }
         }
-        public void mateAnimal (Player player){
-            for (var a : player.animals) {
-                // Add animal to specific Arraylist of same class.
-                // NOTE: Check if both male and female is contained in list.
-                // If yes, try to mate animals.
-                if (a instanceof Horse) {
-                    horseList.add((Horse) a);
-                }
-                if (a instanceof Cow) {
-                    cowList.add((Cow) a);
-                }
-                if (a instanceof Pig) {
-                    pigList.add((Pig) a);
-                }
-                if (a instanceof Sheep) {
-                    sheepList.add((Sheep) a);
-                }
-                if (a instanceof Llama) {
-                    llamaList.add((Llama) a);
-                }
-            }
-        }
+    public void mateAnimal (Player player){ }
+    public String prompt(String question){
+        System.out.println(question);
+        return scanner.next();
     }
+}
+
 
