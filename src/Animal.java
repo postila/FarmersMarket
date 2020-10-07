398e75580f768483ddfd02bc268722c225720a18
postila <isabella.postila@hotmail.com>
2020-10-07 11:34:13 +0200

    Cleaned code and added method increasing health depending on kilos of food feed.

--- src/Animal.java
+++ src/Animal.java
@@ -18,34 +18,30 @@ public abstract class Animal {
         this.gender = Gender.valueOf(gender.toUpperCase());
     }
 
-    public void living() {
-        if (health > 0) {
-            System.out.println("Your animal is still alive.");
-            return;
+    public void animalDie() {
+        if (health <= 0) {
+            isAlive = false;
+            System.out.println("Your animal is dead.");
         }
-        isAlive = false;
-        System.out.println("Your animal is dead.");
     }
     public void changeHealth(){
         health -= (healthDecrease);
     }
-
-    // randomize a percentage between 10-30 for health loss
     public void decreaseHealth(){
-        healthDecrease = random.nextInt(21) + 10;
-        //  Convert the unit of loss into percentage
-        healthDecrease /= 100;
-        // Calculates how much loss an animal should lose
-        healthDecrease = Math.round(health * healthDecrease);
+        healthDecrease = random.nextInt(21) + 10;  // randomize a percentage between 10-30 for health loss
+        healthDecrease /= 100;   //  Convert the unit of loss into percentage
+        healthDecrease = (int)(health * healthDecrease);   // Calculates how much loss an animal should lose
         if(healthDecrease < 1){
             healthDecrease = 1; // So that animal life value always lose something
         }
-        System.out.println("The amount of life that should be lost if animal not fed: " + (int)healthDecrease);
     }
-    public void increaseHealth(){
+    public void increaseHealth(int kilo){
         if(health <= 100){
-        health = (int)health * 1.1;
-            if(health > 100){
+            double increasePerKilo = (0.1 * kilo) + 1.0;
+            System.out.println(increasePerKilo + " percentage based on amount of food");
+            health = (int)health * increasePerKilo;
+            System.out.println(health + " that will be increased.");
+                if(health > 100){
                 health = 100;
             }
         }
@@ -53,25 +49,6 @@ public abstract class Animal {
     public int getPrice(){
         return price;
     }
-
-    public String getName(){
-        return this.name;
-    }
-
-    public Animal mateTwoAnimals(Animal animal) {
-            if (animal.equals(this)) {
-                System.out.println("I can not mate with myself!");
-                return null;
-            }
-            if (animal.getClass().equals(this.getClass()) && animal.gender != this.gender) {
-                boolean yesNo = random.nextBoolean();
-                if (!yesNo) {
-                    System.out.println("Unsuccessful!");
-                    return null;
-                } else {
-                    System.out.println("Mating was successful! Chose a name: ");
-                }
-            }
-        return null;    // If the mating was unsuccessful
+    public void mateTwoAnimals() {
     }
 }
