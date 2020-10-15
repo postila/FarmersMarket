e85e80cb493847b5693d2ee8ebe069b9e1043104
postila <isabella.postila@hotmail.com>
2020-10-15 13:42:58 +0200

    Modified output

--- src/Store.java
+++ src/Store.java
@@ -6,36 +6,41 @@ public class Store {
     public void createAnimal(Player player){
         boolean exit = false;
         while(!exit) {
-            clear();
             int animalChoice = 0;
-            System.out.println("\t[ ANIMALS FOR SALE ]" +
-                    "\n[1] HORSE \t|  50.000 SEK" +
-                    "\n[2]  COW \t|  12.000 SEK" +
-                    "\n[3]  PIG \t|   1.500 SEK" +
-                    "\n[4] SHEEP \t|   1.000 SEK" +
-                    "\n[5] LLAMA \t|  10.000 SEK" +
-                    "\n[6]\t\tEXIT STORE  ");
+            print("\n".repeat(10) + "-".repeat(56) +
+                    "\n\t\t\tWELCOME TO FARMERS MARKETS\n" +
+                    "-".repeat(56) +
+                    "\n    ANIMALS        PRICE       MAX AGE" +
+                    "\n[1]  HORSE \t|   50.000 SEK\t|   20 YRS" +
+                    "\n[2]   COW  \t|   12.000 SEK\t|   10 YRS" +
+                    "\n[3]   PIG  \t|    1.500 SEK\t|    8 YRS" +
+                    "\n[4]  SHEEP \t|    1.000 SEK\t|   10 YRS" +
+                    "\n[5]  LLAMA \t|   10.000 SEK\t|   12 YRS" +
+                    "\n\n[6]\t EXIT STORE ");
             try {
-                animalChoice = Integer.parseInt(prompt("\nBuy animal or enter [6] to leave the store, " + player.name + "."));
+                animalChoice = Integer.parseInt(prompt("-".repeat(56) +
+                        "\nWhat animal would you like to buy today " + player.niceName() + "?\n" +
+                        "-".repeat(56)));
             } catch (Exception ignore){}
 
             switch (animalChoice) {
                 case 1 -> { player.addAnimal(new Horse(setName(), setGender()));
-                print("Congratulations, you just bought a horse!");
+                print(" Congratulations, you bought a horse!");
                 sleep(1000);}
                 case 2 -> { player.addAnimal(new Cow(setName(),setGender()));
-                print("Congratulations, you just bought a new cow!");
+                print(" Congratulations, you bought a cow!");
                 sleep(1000);}
                 case 3 -> { player.addAnimal(new Pig(setName(), setGender()));
-                print("Congratulations, you just bought a new pig!");
+                print(" Congratulations, you bought a pig!");
                 sleep(1000);}
                 case 4 -> { player.addAnimal(new Sheep(setName(), setGender()));
-                print("Congratulations, you just bought a new sheep!");
+                print(" Congratulations, you bought a sheep!");
                 sleep(1000);}
                 case 5 -> {player.addAnimal(new Llama(setName(),setGender()));
-                print("Congratulations, you just bought a new llama!");
+                print(" Congratulations, you bought a llama!");
                 sleep(1000);}
-                case 6 -> {System.out.println("Come back soon, " + player.name + "!");
+                case 6 -> {print("-".repeat(56) +
+                        "\n\t\tWELCOME BACK ANOTHER TIME, " + player.name + "!");
                 exit = true;}
                 default -> {System.out.println("ERROR : Choose an option between 1-6!");
                 sleep(1000);}
@@ -47,22 +52,31 @@ public class Store {
         int foodChoice = 0;
         int kilos = 0;
         while (!exit){
-            clear();
-            System.out.println("\t\t[ FOOD ] " +
-                    "\n[1] HAY\t\t|  100 SEK/KG" +
-                    "\n[2] GRASS\t|  150 SEK/KG" +
-                    "\n[3] GRAIN\t|  200 SEK/KG" +
-                    "\n[4] \tEXIT STORE");
+            print("\n".repeat(10) + "-".repeat(56) +
+                    "\n\t\t\tFARMERS MARKETS FOOD COURT  \n" +
+                    "-".repeat(56) +
+                    "\n\t FOODS     PRICE           ANIMALS" +
+                    "\n[1]\t HAY\t|  100 SEK/KG\t|  HORSE  LLAMA  SHEEP" +
+                    "\n[2]\t GRASS\t|  150 SEK/KG\t|  HORSE  COW" +
+                    "\n[3]\t GRAIN\t|  200 SEK/KG\t|  LLAMA  SHEEP  PIG" +
+                    "\n\n[4]\tEXIT STORE");
         do {
             try {
-                foodChoice = Integer.parseInt(prompt("What food would you like to buy today?"));
+                foodChoice = Integer.parseInt(prompt("-".repeat(56) +
+                        "\nWhat food would you like to buy today " + player.niceName() + "?\n" +
+                        "-".repeat(56)));
+                if(foodChoice == 4){
+                    break;
+                }
             } catch (Exception e) {
                 print("ERROR : You have to chose a food option between 1-3.");
                 sleep(1000);
             }
             if (foodChoice >= 1 && foodChoice <= 3) {
                 try {
-                    kilos = Integer.parseInt(prompt("How many kilos would you like to get?"));
+                    kilos = Integer.parseInt(prompt("-".repeat(56) +
+                            "\nHow many kilos would you like to get?\n" +
+                            "-".repeat(56)));
                 } catch (Exception e) {
                     print("ERROR : Register amount of kilos with numbers.");
                     sleep(1000);
@@ -73,7 +87,8 @@ public class Store {
                 case 1 -> player.addFood(new Hay(kilos));
                 case 2 -> player.addFood(new Grass(kilos));
                 case 3 -> player.addFood(new Grain(kilos));
-                case 4 -> {System.out.println("Welcome back another time!");
+                case 4 -> {System.out.println("-".repeat(56) +
+                        "\n\t\tWELCOME BACK ANOTHER TIME!");
                     exit = true;}
                 default -> {System.out.println("Choose an option between 1-4!");
                 sleep(1000);}
@@ -97,13 +112,13 @@ public class Store {
         } catch (Exception ignore){}
     }
     public static String setName(){     // Help method
-        return prompt("[ ENTER A NAME ]");
+        return prompt("\t".repeat(3) + "[ GIVE YOUR ANIMAL A NAME ]");
     }
     public static String setGender(){   // Help method
         var input = "";
         while(true) {
             try {
-                input = prompt("[ GENDER ]\n[F]  Female\n[M]  Male");
+                input = prompt("\t".repeat(3) + "[ CHOOSE GENDER ]\n[F]  Female\n[M]  Male");
                 if(input.toUpperCase().equals("F") || input.toUpperCase().equals("M")){
                     break;
                 }
